cee1aec2a36233ed34c7ffcb1a0b3d36a349cd28
proS1996 <sonipronaldo1996@gmail.com>
2024-08-24 20:03:59 +0530

    feat : create social icons and add image

---
+++ app/page.jsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+//components
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I&apos;m <br />
+              <span className="text-accent">Vijay Chandoliya</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              excel at crafting elegant digital experiences and i am proficient
+              in various programming languages and technologies{" "}
+            </p>
+            {/* button and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-8">
+                <Socials
+                  containerStyles={"flex gap-6"}
+                  iconStyles={
+                    "w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  }
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
